ac117d5898c7ed9bb747785b693d940d396d97d7
nguyenthuyenlonghcm <nguyenthuyenlonghcm@gmail.com>
2026-01-14 11:44:14 +0700

    Addition more 1 testcase

--- src/test/java/com/log/selenium/GoogleTest.java
+++ src/test/java/com/log/selenium/GoogleTest.java
@@ -5,10 +5,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleTest extends BaseTest {
+    /**
+     * Test 1: Verify title (cố tình FAIL để demo report)
+     */
     @Test
     public void verifyGoogleTitle() {
         getDriver().get("https://www.google.com");
         String title = getDriver().getTitle();
         Assert.assertEquals(title, "Google");
+    }
+
+    /**
+     * Test 2: Verify current URL (PASS)
+     */
+    @Test
+    public void verifyGoogleUrl() {
+
+        getDriver().get("https://www.google.com");
+
+        String currentUrl = getDriver().getCurrentUrl();
+
+        Assert.assertTrue(
+                currentUrl.contains("google"),
+                "Current URL does not contain 'google'"
+        );
     }
 }
