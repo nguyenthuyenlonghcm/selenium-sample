6b4777e0def1192ade5194ced7e2bdf8827e43c4
nguyenthuyenlonghcm <nguyenthuyenlonghcm@gmail.com>
2026-01-14 13:39:52 +0700

    Demo tescase Fail

--- src/test/java/com/log/selenium/GoogleTest.java
+++ src/test/java/com/log/selenium/GoogleTest.java
@@ -12,7 +12,7 @@ public class GoogleTest extends BaseTest {
     public void verifyGoogleTitle() {
         getDriver().get("https://www.google.com");
         String title = getDriver().getTitle();
-        Assert.assertEquals(title, "Google");
+        Assert.assertEquals(title, "Google999");
     }
 
     /**
